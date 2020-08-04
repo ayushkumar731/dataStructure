fb3514731f3a8fd6f27b2784f2a4996cf7a78b01
Ayush Kumar <kumarayush731@gmail.com>
2020-08-04 18:58:11 +0530

    que  of mid point  linked list added

--- linkedList/midPointOfLL.java
+++ linkedList/midPointOfLL.java
@@ -1,3 +1,19 @@
+/*
+Mid Point Linked List
+
+Given a linked list, find and return the midpoint.
+If the length of linked list is even, return the first mid point.
+Input format : Linked list elements (separated by space and terminated by -1)`
+
+Sample Input 1 :
+1 2 3 4 5 -1
+Sample Output 1 :
+3
+Sample Input 2 :
+1 2 3 4 -1
+Sample Output 2 :
+2
+ */
 package linkedList;
 import java.util.Scanner;
 public class midPointOfLL {
