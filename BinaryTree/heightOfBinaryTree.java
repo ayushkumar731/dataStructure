a53f494ce0769656bbd0651b32a28f0368b9aa3b
Ayush Kumar <kumarayush731@gmail.com>
2020-08-06 09:13:56 +0530

    height of binary tree

---
+++ BinaryTree/heightOfBinaryTree.java
@@ -0,0 +1,73 @@
+/*
+Height Of Tree
+
+Given a binary tree, find and return the height of given tree.
+Input format :
+Nodes in the level order form (separated by space). If any node does not have left or right child, take -1 in its place
+Output format :
+Height
+Constraints :
+1 <= N <= 10^5
+Sample Input :
+10
+ 9 
+4 
+-1 
+-1 
+ 5 
+ 8 
+-1 
+6 
+-1 
+-1 
+3 
+-1 
+-1
+-1
+Sample Output :
+5
+ */
+package BinaryTree;
+import java.util.Scanner;
+public class heightOfBinaryTree {
+    
+    private static int height(BinaryTreeNode<Integer> root) {
+        if(root==null){
+            return 0;
+        }
+        int heightLeft=height(root.left);
+        int heightRight=height(root.right);
+        if(heightLeft>=heightRight){
+            return 1+heightLeft;
+        }else{
+            return 1+heightRight;
+        }
+    }
+    private static BinaryTreeNode<Integer> takeInput(boolean isRoot,int data,boolean isLeft) {
+        if(isRoot){
+            System.out.println("Enter the parentData");
+        }else{
+            if(isLeft){
+                System.out.println("Enter the left child of :"+data);
+            }else{
+             System.out.println("Enter the right child of :"+data);
+            }
+        }
+        Scanner s=new Scanner(System.in);
+        int rootData=s.nextInt();
+        if(rootData==-1){
+             return null;
+        }
+        BinaryTreeNode<Integer> root=new BinaryTreeNode<Integer>(rootData);
+        BinaryTreeNode<Integer> left=takeInput(false, rootData,true);
+        BinaryTreeNode<Integer> right=takeInput(false, rootData,false);
+        root.left=left;
+        root.right=right;
+        return root;
+     }
+     public static void main(String[] args) {
+         BinaryTreeNode<Integer> root=takeInput(true,0,true);
+        System.out.println(height(root));
+     }
+
+}
