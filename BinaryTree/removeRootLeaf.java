92f9b06c54355a9b1d5a47e3464fa92b1bef9f96
Ayush Kumar <kumarayush731@gmail.com>
2020-08-06 15:56:20 +0530

    remove root leaf

--- BinaryTree/removeRootLeaf.java
+++ BinaryTree/removeRootLeaf.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 public class removeRootLeaf {
     private static BinaryTreeNode<Integer> removeLeaf(BinaryTreeNode<Integer> root) {
         if(root==null){
@@ -17,38 +19,58 @@ public class removeRootLeaf {
         if(root==null){
             return;
         }
-        System.out.print(root.data+":");
-        if(root.left!=null){
-            System.out.print("L"+root.left.data+",");
+        Queue<BinaryTreeNode<Integer>> pendingChildren =new LinkedList<BinaryTreeNode<Integer>>();
+        pendingChildren.add(root);
+        while(!pendingChildren.isEmpty()){
+            BinaryTreeNode<Integer> front=pendingChildren.poll();
+            System.out.print(front.data+":");
+            if(front.left!=null){
+                System.out.print("L:"+front.left.data+",");
+                pendingChildren.add(front.left);
+            }
+            if(front.left==null){
+                System.out.print("L:"+-1+",");
+            }
+            if(front.right!=null){
+                System.out.print("R:"+front.right.data);
+                pendingChildren.add(front.right);
+            }
+            if(front.right==null){
+                System.out.print("R:"+-1);
+            }
+            System.out.println();
         }
-        if(root.right!=null){
-            System.out.print("R"+root.right.data);
-        }
+
     }
-    private static BinaryTreeNode<Integer> takeInput(boolean isRoot,int data,boolean isLeft) {
-        if(isRoot){
-            System.out.println("Enter the parentData");
-        }else{
-            if(isLeft){
-                System.out.println("Enter the left child of :"+data);
-            }else{
-             System.out.println("Enter the right child of :"+data);
+    private static BinaryTreeNode<Integer> takeInput() {
+        Scanner s=new Scanner(System.in);
+        int data= s.nextInt();
+        if(data==-1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root=new BinaryTreeNode<Integer>(data);
+        Queue<BinaryTreeNode<Integer>> q=new LinkedList<BinaryTreeNode<Integer>>();
+        q.add(root);
+        while(!q.isEmpty()){
+            BinaryTreeNode<Integer> front=q.poll();
+            int left=s.nextInt();
+            if(left!=-1){
+                BinaryTreeNode<Integer> leftChild=new BinaryTreeNode<Integer>(left);
+                front.left=leftChild;
+                q.add(leftChild);
+            }
+            int right=s.nextInt();
+            if(right!=-1){
+                BinaryTreeNode<Integer> rightChild=new BinaryTreeNode<Integer>(right);
+                front.right=rightChild;
+                q.add(rightChild);
             }
         }
-        Scanner s=new Scanner(System.in);
-        int rootData=s.nextInt();
-        if(rootData==-1){
-             return null;
-        }
-        BinaryTreeNode<Integer> root=new BinaryTreeNode<Integer>(rootData);
-        BinaryTreeNode<Integer> left=takeInput(false, rootData,true);
-        BinaryTreeNode<Integer> right=takeInput(false, rootData,false);
-        root.left=left;
-        root.right=right;
         return root;
+
      }
      public static void main(String[] args) {
-        BinaryTreeNode<Integer> root=takeInput(true,0,true);
+        BinaryTreeNode<Integer> root=takeInput();
         root=removeLeaf(root);
         print(root);
      }    
