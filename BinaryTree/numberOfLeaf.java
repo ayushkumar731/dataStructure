e74e2af02d87ebe506477d6ef45b5fc6da744f70
Ayush Kumar <kumarayush731@gmail.com>
2020-08-06 14:58:12 +0530

    number of leaf in binary tree

--- BinaryTree/numberOfLeaf.java
+++ BinaryTree/numberOfLeaf.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
 public class numberOfLeaf {
     private static int numLeaf(BinaryTreeNode<Integer> root) {
         if(root==null){
@@ -13,30 +15,35 @@ public class numberOfLeaf {
             return leftLeafCount+rightLeafCount;
         }
     }
-    private static BinaryTreeNode<Integer> takeInput(boolean isRoot,int data,boolean isLeft) {
-        if(isRoot){
-            System.out.println("Enter the parentData");
-        }else{
-            if(isLeft){
-                System.out.println("Enter the left child of :"+data);
-            }else{
-             System.out.println("Enter the right child of :"+data);
+    private static BinaryTreeNode<Integer> takeInput() {
+        Scanner s=new Scanner(System.in);
+        int data= s.nextInt();
+        if(data==-1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root=new BinaryTreeNode<Integer>(data);
+        Queue<BinaryTreeNode<Integer>> q=new LinkedList<BinaryTreeNode<Integer>>();
+        q.add(root);
+        while(!q.isEmpty()){
+            BinaryTreeNode<Integer> front=q.poll();
+            int left=s.nextInt();
+            if(left!=-1){
+                BinaryTreeNode<Integer> leftChild=new BinaryTreeNode<Integer>(left);
+                front.left=leftChild;
+                q.add(leftChild);
+            }
+            int right=s.nextInt();
+            if(right!=-1){
+                BinaryTreeNode<Integer> rightChild=new BinaryTreeNode<Integer>(right);
+                front.right=rightChild;
+                q.add(rightChild);
             }
         }
-        Scanner s=new Scanner(System.in);
-        int rootData=s.nextInt();
-        if(rootData==-1){
-             return null;
-        }
-        BinaryTreeNode<Integer> root=new BinaryTreeNode<Integer>(rootData);
-        BinaryTreeNode<Integer> left=takeInput(false, rootData,true);
-        BinaryTreeNode<Integer> right=takeInput(false, rootData,false);
-        root.left=left;
-        root.right=right;
         return root;
+
      }
      public static void main(String[] args) {
-         BinaryTreeNode<Integer> root=takeInput(true,0,true);
+         BinaryTreeNode<Integer> root=takeInput();
         System.out.println(numLeaf(root));
      }
 
